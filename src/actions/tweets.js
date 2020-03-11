d03f1f77e6f7749cc5dfd7857a01708cc12af534
Liu, Charles <charles.liu@bhp.com>
2020-03-11 14:33:48 +0800

    tweet pages

---
+++ src/actions/tweets.js
@@ -0,0 +1,34 @@
+import { saveLikeToggle } from '../utils/api'
+
+export const RECEIVE_TWEETS = 'RECEIVE_TWEETS'
+export const TOGGLE_TWEET = 'TOGGLE_TWEET'
+
+export function receiveTweets (tweets) {
+  return {
+    type: RECEIVE_TWEETS,
+    tweets,
+  }
+}
+
+export function handleToggleTweet (info) {
+  return (dispatch) => {
+
+    dispatch(toggleTweet(info))
+
+    return saveLikeToggle(info)
+      .catch((e) => {
+        console.warn('Error in handleToggleTweet: ', e)
+        dispatch(toggleTweet(info))
+        alert('The was an error liking the tweet. Try again.')
+      })
+  }
+}
+
+function toggleTweet ({ id, authedUser, hasLiked }) {
+  return {
+    type: TOGGLE_TWEET,
+    id,
+    authedUser,
+    hasLiked
+  }
+}
